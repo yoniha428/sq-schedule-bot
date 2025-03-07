4813d085b410ce3fba5b26a3c159a1f5f5a3c8e2
yoniha428 <superrope428@gmail.com>
2025-03-07 20:52:27 +0900

    add button interaction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -16,10 +17,9 @@ client.once('ready', async() => {
 client.on('messageCreate', async (message) => {
   if (message.author.id === client.user.id) return;
   if (message.channel.name !== 'sq-schedule') return;
+  let obj = JSON.parse(fs.readFileSync('./log.json', 'utf8'));
   const channel = message.channel;
-  let content = message.content;
-  content = content.split('\n');
-  content = content.filter(s => s[0] !== '@');
+  let content = message.content.split('\n').filter(s => s[0] !== '@');
   for(const s of content){
     const time = new Date(parseInt(s.substring(20, 30)) * 1000);
     await channel.send(
@@ -34,14 +34,40 @@ client.on('messageCreate', async (message) => {
       .setLabel('Can join')
       .setStyle(ButtonStyle.Primary);
     const row = new ActionRowBuilder().addComponents(button);
-    await channel.send({ components: [row] });
+    const sendRow = await channel.send({ components: [row] });
+    obj.log.push({id: sendRow.id, count: 0, time: time.getMilliseconds()});
   }
+  fs.writeFileSync('./log.json', JSON.stringify(obj, undefined, ' '));
 });
 
 client.on('interactionCreate', async (interaction) => {
   if(interaction.isButton()){
-
+    // console.log('Button pushed');
+    if(interaction.customId === 'vote'){
+      console.log(interaction.message.id);
+      let obj = JSON.parse(fs.readFileSync('./log.json', 'utf8'));
+      for(let log of obj.log) if(log.id === interaction.message.id) log.count++;
+      fs.writeFileSync('./log.json', JSON.stringify(obj, undefined, ' '));
+      const button = new ButtonBuilder()
+        .setCustomId('drop')
+        .setLabel('Drop')
+        .setStyle(ButtonStyle.Secondary);
+      const row = new ActionRowBuilder().addComponents(button);
+      interaction.update({content: "Successfully joined", components: [row]});
+    }
+    if(interaction.customId === 'drop'){
+      console.log(interaction.message.id);
+      let obj = JSON.parse(fs.readFileSync('./log.json', 'utf8'));
+      for(let log of obj.log) if(log.id === interaction.message.id) log.count--;
+      fs.writeFileSync('./log.json', JSON.stringify(obj, undefined, ' '));
+      const button = new ButtonBuilder()
+        .setCustomId('join')
+        .setLabel('Can join')
+        .setStyle(ButtonStyle.Primary);
+      const row = new ActionRowBuilder().addComponents(button);
+      interaction.update({content: "Successfully dropped", components: [row]});
+    }
   }
-})
+});
 
 client.login(token);
