50805e8e8b23108ef39acb302e6f9721796790a2
yoniha428 <superrope428@gmail.com>
2025-03-17 17:30:33 +0900

    sendNotify関数を作成し、全体をリファクタ

--- index.js
+++ index.js
@@ -32,14 +32,29 @@ const server = http.createServer((_req, res) => {
 const port = process.env.PORT || 10000;
 server.listen(port);
 
-// 送信者だけに見えるreplyを作成
-function ephemeralReply(interaction, content) {
-  interaction.reply({ content: content, flags: MessageFlags.Ephemeral });
+async function sendNotify(log, message, channel) {
+  console.log("trying to send notification");
+  let sendMessage = "";
+  for (const userId of log.participants) {
+    sendMessage += "<@" + userId + ">";
+  }
+  sendMessage += "\n";
+  sendMessage += message;
+  const result = await channel.send(sendMessage);
+  return new Promise((resolve, reject) => {
+    if (result) return resolve(message);
+    else {
+      console.log("send message failed");
+      return reject(null);
+    }
+  });
 }
 
 // 毎分走る、通知用
 cron.schedule("* * * * *", async () => {
   console.log("cron running");
+
+  // ログインしていないなら、ログインする
   if (!client.isReady()) {
     console.log("not logged in");
     await client.login(discordToken);
@@ -48,49 +63,50 @@ cron.schedule("* * * * *", async () => {
       return;
     }
   }
-  let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
-  const now = new Date().getTime();
+
+  // 送信用チャンネルを準備する
   const notifyChannel = client.channels.resolve(notifyChannelId);
   if (!notifyChannel || notifyChannel.type !== ChannelType.GuildText) {
     console.log("notifyChannel not found or is not Text channel");
     return;
   }
+
+  let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
+  const now = new Date().getTime();
+
   obj = obj.filter((log) => log.time > now);
   console.log(obj);
+
   for (let log of obj) {
-    if (log.count < log.format) continue;
-    if (now + 60 * 60 * 1000 > log.time && log.notified.in60min === 0) {
-      console.log('trying to send notify\n', log);
-      let message = "";
-      for (const id of log.participants) {
-        message += (await client.users.fetch(id)).toString();
-      }
-      message += "\n60分後に模擬があります！\n!cして！";
-      if (log.count > log.format) {
-        message += "\n人数超過です！話し合って参加者を決めましょう！";
-      }
-      const result = await notifyChannel.send(message);
-      if (result) {
-        log.notified.in60min = 1;
-      } else {
-        console.log("message send failed");
-      }
+
+    // 人が足りない
+    if (log.count < log.format) {
+      continue;
     }
+
+    // 何分後に模擬があるか
+    const minutesAfter = new Date(log.time - now + 30 * 1000).getMinutes();
+
+    // 60分後通知
+    if (now + 60 * 60 * 1000 > log.time && log.notified.in60min === 0) {
+      const message =
+        minutesAfter +
+        "分後に模擬があります！\n!cして！" +
+        (log.count > log.format
+          ? "\n人数超過です！話し合って参加者を決めましょう！"
+          : "");
+      const result = await sendNotify(log, message, notifyChannel);
+      if (result) log.notified.in60min = 1;
+    }
+
+    // 30分後通知
     if (now + 30 * 60 * 1000 > log.time && log.notified.in30min === 0) {
-      console.log("trying to send notify\n", log);
-      let message = "";
-      for (const id of log.participants) {
-        message += (await client.users.fetch(id)).toString();
-      }
-      message += "\n30分後に模擬があります！\n!cした？";
-      const result = await notifyChannel.send(message);
-      if (result) {
-        log.notified.in30min = 1;
-      } else {
-        console.log("message send failed");
-      }
+      const message = minutesAfter + "分後に模擬があります！\n!cした？";
+      const result = await sendNotify(log, message, notifyChannel);
+      if (result) log.notified.in30min = 1;
     }
   }
+
   fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
 });
 
@@ -176,45 +192,52 @@ client.on("messageCreate", async (message) => {
 client.on("interactionCreate", async (interaction) => {
   //ボタンのとき
   if (interaction.isButton()) {
+
+    // 考え中のリプライ
     await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-    // joinボタンのとき
+    // joinボタン
     if (interaction.customId === "join") {
-      // log.jsonからobjに読み取る
+      
+      // idが一致するログを抽出
       let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
+      const matchLog = obj.filter((log) => log.id === interaction.message.id);
 
-      let matchLog;
-      const userId = interaction.member.user.id;
-      for (let log of obj)
-        if (log.id === interaction.message.id) {
-          if (matchLog) {
-            interaction.editReply({
-              content:
-                "一致するメッセージIDがログに2つ以上あります。\n管理者に連絡してください。",
-            });
-            return;
-          }
-          if (log.participants.includes(userId)) {
-            interaction.editReply({ content: "既に参加しています" });
-            return;
-          }
-          log.count++;
-          log.participants.push(userId);
-          matchLog = log;
-        }
-      if (!matchLog) {
-        // console.log(obj);
+      // 2個以上マッチ
+      if (matchLog.length > 1) {
         interaction.editReply({
           content:
-            "メッセージIDに一致するログが見つかりませんでした。\n現在時刻以降の投票にも関わらずこのメッセージが表示されている場合は管理者に連絡してください。",
+            "一致するメッセージIDがログに2つ以上あります。\n管理者に連絡してください。",
         });
         return;
       }
 
-      fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
+      // マッチなし
+      if (matchLog.length === 0) {
+        interaction.editReply({
+          content:
+            "メッセージIDに一致するログが見つかりませんでした。\n" +
+            "現在時刻以降の投票にも関わらずこのメッセージが表示されている場合は管理者に連絡してください。",
+        });
+        return;
+      }
 
+      const log = matchLog.at(0);
+      const userId = interaction.member.user.id;
+
+      // 参加済み
+      if (log.participants.includes(userId)) {
+        interaction.editReply({ content: "既に参加しています" });
+        return;
+      }
+
+      // 正常な処理
+      log.count++;
+      log.participants.push(userId);
+
+      // メッセージ編集
       let participantsName = [];
-      for (const id of matchLog.participants) {
+      for (const id of log.participants) {
         const username = (await client.users.fetch(id)).username;
         // console.log(username);
         participantsName.push(username);
@@ -223,68 +246,86 @@ client.on("interactionCreate", async (interaction) => {
         "参加者: " +
           participantsName.join(", ") +
           "\n参加人数: " +
-          matchLog.count +
+          log.count +
           "/" +
-          matchLog.format
+          log.format
       );
       interaction.deleteReply();
       interaction.message.edit({ content: content });
-    }
-    if (interaction.customId === "drop") {
-      // console.log(interaction.message.id);
-      let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
 
-      let matchLog;
-      const userId = interaction.member.user.id;
-      for (let log of obj)
-        if (log.id === interaction.message.id) {
-          if (matchLog) {
-            interaction.editReply({
-              content:
-                "一致するメッセージIDがログに2つ以上あります。\n管理者に連絡してください。",
-            });
-            return;
-          }
-          if (!log.participants.includes(userId)) {
-            interaction.editReply({ content: "joinしていません" });
-            return;
-          }
-          log.count--;
-          log.participants = log.participants.filter((id) => id !== userId);
-          matchLog = log;
-        }
-      if (!matchLog) {
-        // console.log(obj);
+      // ログを更新
+      fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
+    }
+
+    // dropボタン
+    if (interaction.customId === "drop") {
+      // idが一致するログを抽出
+      let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
+      const matchLog = obj.filter((log) => log.id === interaction.message.id);
+
+      // 2個以上マッチ
+      if (matchLog.length > 1) {
         interaction.editReply({
           content:
-            "メッセージIDに一致するログが見つかりませんでした。\n現在時刻以降の投票にも関わらずこのメッセージが表示されている場合は管理者に連絡してください。",
+            "一致するメッセージIDがログに2つ以上あります。\n管理者に連絡してください。",
         });
         return;
       }
-      fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
+
+      // マッチなし
+      if (matchLog.length === 0) {
+        interaction.editReply({
+          content:
+            "メッセージIDに一致するログが見つかりませんでした。\n" +
+            "現在時刻以降の投票にも関わらずこのメッセージが表示されている場合は管理者に連絡してください。",
+        });
+        return;
+      }
+
+      const log = matchLog.at(0);
+      const userId = interaction.member.user.id;
+
+      // 参加していない
+      if (!log.participants.includes(userId)) {
+        interaction.editReply({ content: "参加していません" });
+        return;
+      }
+
+      // 正常な処理
+      log.count--;
+      log.participants = log.participants.filter(userId);
+
+      // メッセージ編集
       let participantsName = [];
-      for (const id of matchLog.participants) {
+      for (const id of log.participants) {
         const username = (await client.users.fetch(id)).username;
-        console.log(username);
+        // console.log(username);
         participantsName.push(username);
       }
       const content = escapeMarkdown(
         "参加者: " +
           participantsName.join(", ") +
           "\n参加人数: " +
-          matchLog.count +
+          log.count +
           "/" +
-          matchLog.format
+          log.format
       );
       interaction.deleteReply();
       interaction.message.edit({ content: content });
+
+      // ログを更新
+      fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
     }
   }
 });
 
+
+// log.jsonがないなら作る
 if (!fs.existsSync("log.json")) {
   fs.writeFileSync("log.json", JSON.stringify([], undefined, " "));
 }
 
-await client.login(discordToken);
-if (!client.isReady()) console.log("login failed");
+const result = await client.login(discordToken);
+if (!result){
+  throw new Error("login failed");
+}
