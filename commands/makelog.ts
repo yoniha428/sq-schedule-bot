b6ffcfaacb02694288578c0edb1296084fb1f1c5
yoniha428 <superrope428@gmail.com>
2025-03-18 22:08:31 +0900

    makelog.tsを作って分割

---
+++ commands/makelog.ts
@@ -0,0 +1,76 @@
+import {
+  OmitPartialGroupDMChannel,
+  Message,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} from "discord.js";
+
+export const makeLog = async (
+  message: OmitPartialGroupDMChannel<Message<boolean>>,
+  obj: any
+) => {
+  const content = message.content
+    .split("\n")
+    .filter((s) => s.substring(0, 9) !== "@everyone");
+  const channel = message.channel;
+  const needFormat = obj.needFormat;
+
+  for (const s of content) {
+    // unixTimeとjstTimeをゴリ押しで作って、formatも取得
+    // log.jsonで管理するのはunixTime
+    const unixTime = parseInt(s.substring(20, 30)) * 1000;
+    const jstTime = unixTime + 9 * 60 * 60 * 1000;
+    const time = new Date(jstTime);
+    const format = parseInt(s.at(10) ?? "0");
+
+    // 投票を作る必要がないフォーマットなら飛ばす
+    if (!needFormat.includes(format)) continue;
+
+    // 日程のメッセージを送る
+    const result = await channel.send(
+      "# " +
+      String(time.getMonth() + 1) +
+      "/" +
+      String(time.getDate()) +
+      " " +
+      String(time.getHours()).padStart(2, "0") +
+      ":" +
+      String(time.getMinutes()).padStart(2, "0") +
+      " " +
+      s.substring(10, 13)
+    );
+
+    if(!result) console.log("message sending failed");
+
+    // joinとdropのボタンを作って送る
+    const button1 = new ButtonBuilder()
+      .setCustomId("join")
+      .setLabel("Can join")
+      .setStyle(ButtonStyle.Success);
+    const button2 = new ButtonBuilder()
+      .setCustomId("drop")
+      .setLabel("drop")
+      .setStyle(ButtonStyle.Danger);
+    const row = new ActionRowBuilder<ButtonBuilder>()
+      .addComponents(button1)
+      .addComponents(button2);
+    const sendResult = await channel.send({ components: [row] });
+
+    // logに現在の模擬の情報をpushする
+    obj.logs.push({
+      id: sendResult.id,
+      time: unixTime,
+      format: format,
+      count: 0,
+      participants: [],
+      notified: {
+        in30min: 0,
+        in60min: 0,
+      },
+    });
+  }
+
+  // log.jsonにobjを書き込む
+  return obj
+};
--- index.ts
+++ index.ts
@@ -16,11 +16,12 @@ import http from "http";
 
 // 設定のimport
 import config from "./config.json" with { type: "json" };
-const { discordToken, notifyChannelId, noNeedFormat } = config;
+const { discordToken, notifyChannelId } = config;
 
 // 自作コマンドのimport
 import { joinCommand } from "./commands/join.js";
 import { dropCommand } from "./commands/drop.js";
+import { makeLog } from "./commands/makelog.js";
 
 const client = new Client({
   intents: [
@@ -125,75 +126,103 @@ client.on(Events.ClientReady, async () => {
 // 自分がいるサーバーにメッセージが送信されたとき
 client.on(Events.MessageCreate, async (message) => {
   // sq-scheduleに送られた、このbot以外の発言で、@everyoneしているものを拾う
-  if (message.author.id === client.user?.id) return;
-  if (message.channel.type !== ChannelType.GuildText) return;
-  if (message.channel.name !== "sq-schedule") return;
-  if (message.content.substring(0, 9) != "@everyone") return;
+  // if (message.author.id === client.user?.id) return;
+  // if (message.channel.type !== ChannelType.GuildText) return;
+  // if (message.content.substring(0, 9) != "@everyone") return;
 
-  // contentの整理と、送信チャンネルの確認
-  const content = message.content
-    .split("\n")
-    .filter((s) => s.substring(0, 9) !== "@everyone");
-  const channel = message.channel;
+  const targetLogName = "./log/" + message.channelId + ".json";
+  const existLog = fs.existsSync(targetLogName);
+  let obj = existLog
+    ? JSON.parse(fs.readFileSync(targetLogName, "utf8"))
+    : {
+        followChannel: message.channel.id,
+        notifyChannel: message.channel.id,
+        needFormat: [2, 3, 4, 6],
+        logs: [],
+      };
 
-  // log.jsonから読み取る
-  let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
-
-  for (const s of content) {
-    // unixTimeとjstTimeをゴリ押しで作って、formatも取得
-    // log.jsonで管理するのはunixTime
-    const unixTime = parseInt(s.substring(20, 30)) * 1000;
-    const jstTime = unixTime + 9 * 60 * 60 * 1000;
-    const time = new Date(jstTime);
-    const format = parseInt(s.at(10) ?? "0");
-
-    // 投票を作る必要がないフォーマットなら飛ばす
-    if (noNeedFormat.includes(format)) continue;
-
-    // 日程のメッセージを送る
-    await channel.send(
-      "# " +
-        String(time.getMonth() + 1) +
-        "/" +
-        String(time.getDate()) +
-        " " +
-        String(time.getHours()).padStart(2, "0") +
-        ":" +
-        String(time.getMinutes()).padStart(2, "0") +
-        " " +
-        s.substring(10, 13)
-    );
-
-    // joinとdropのボタンを作って送る
-    const button1 = new ButtonBuilder()
-      .setCustomId("join")
-      .setLabel("Can join")
-      .setStyle(ButtonStyle.Success);
-    const button2 = new ButtonBuilder()
-      .setCustomId("drop")
-      .setLabel("drop")
-      .setStyle(ButtonStyle.Danger);
-    const row = new ActionRowBuilder<ButtonBuilder>()
-      .addComponents(button1)
-      .addComponents(button2);
-    const sendResult = await channel.send({ components: [row] });
-
-    // logに現在の模擬の情報をpushする
-    obj.push({
-      id: sendResult.id,
-      time: unixTime,
-      format: format,
-      count: 0,
-      participants: [],
-      notified: {
-        in30min: 0,
-        in60min: 0,
-      },
-    });
+  // 自分のものでなく、notifyChannelに送られていて、@everyoneしているもの
+  if (
+    message.author.id !== client.user?.id &&
+    (!existLog || message.channel.id === obj.followChannel)&&
+    message.content.substring(0, 9) === "@everyone"
+  ){
+    if(!existLog){
+      message.channel.send(
+        "初期設定としてこのチャンネルをフォローチャンネルおよびお知らせチャンネルに設定しました。\n" + 
+        "sq-followchannelコマンドおよびsq-notifychannelコマンドにより、設定を行ってください。\n" +
+        "sq-needformatコマンドによって、通知が必要なフォーマットの設定をすることもできます。"
+      );
+      fs.writeFileSync(targetLogName, JSON.stringify(obj, undefined, " "));
+    }
+    obj = await makeLog(message, obj);
+    fs.writeFileSync(targetLogName, JSON.stringify(obj, undefined, " "));
   }
 
-  // log.jsonにobjを書き込む
-  fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
+  // // contentの整理と、送信チャンネルの確認
+  // const content = message.content
+  //   .split("\n")
+  //   .filter((s) => s.substring(0, 9) !== "@everyone");
+  // const channel = message.channel;
+
+  // // log.jsonから読み取る
+  // // let obj = JSON.parse(fs.readFileSync("./log.json", "utf8"));
+
+  // for (const s of content) {
+  //   // unixTimeとjstTimeをゴリ押しで作って、formatも取得
+  //   // log.jsonで管理するのはunixTime
+  //   const unixTime = parseInt(s.substring(20, 30)) * 1000;
+  //   const jstTime = unixTime + 9 * 60 * 60 * 1000;
+  //   const time = new Date(jstTime);
+  //   const format = parseInt(s.at(10) ?? "0");
+
+  //   // 投票を作る必要がないフォーマットなら飛ばす
+  //   if (noNeedFormat.includes(format)) continue;
+
+  //   // 日程のメッセージを送る
+  //   await channel.send(
+  //     "# " +
+  //       String(time.getMonth() + 1) +
+  //       "/" +
+  //       String(time.getDate()) +
+  //       " " +
+  //       String(time.getHours()).padStart(2, "0") +
+  //       ":" +
+  //       String(time.getMinutes()).padStart(2, "0") +
+  //       " " +
+  //       s.substring(10, 13)
+  //   );
+
+  //   // joinとdropのボタンを作って送る
+  //   const button1 = new ButtonBuilder()
+  //     .setCustomId("join")
+  //     .setLabel("Can join")
+  //     .setStyle(ButtonStyle.Success);
+  //   const button2 = new ButtonBuilder()
+  //     .setCustomId("drop")
+  //     .setLabel("drop")
+  //     .setStyle(ButtonStyle.Danger);
+  //   const row = new ActionRowBuilder<ButtonBuilder>()
+  //     .addComponents(button1)
+  //     .addComponents(button2);
+  //   const sendResult = await channel.send({ components: [row] });
+
+  //   // logに現在の模擬の情報をpushする
+  //   obj.push({
+  //     id: sendResult.id,
+  //     time: unixTime,
+  //     format: format,
+  //     count: 0,
+  //     participants: [],
+  //     notified: {
+  //       in30min: 0,
+  //       in60min: 0,
+  //     },
+  //   });
+  // }
+
+  // // log.jsonにobjを書き込む
+  // fs.writeFileSync("./log.json", JSON.stringify(obj, undefined, " "));
 });
 
 // interaction作成時
